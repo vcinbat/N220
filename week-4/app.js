b8c37d81562ff321b374753a2d970e5070f3697d
vcinbat <122567462+vcinbat@users.noreply.github.com>
2024-09-17 17:17:36 -0400

    Added arrays

--- week-4/app.js
+++ week-4/app.js
@@ -6,6 +6,8 @@ function printAnswer(answer) {
   answersPTag.innerHTML += `<br /> ${answer}`;
 }
 
+// ARRAY BASICS
+
 // 1. Arrays will print as a string seperated with commas by default
 answersPTag.innerHTML += studentGrades;
 
@@ -53,3 +55,102 @@ printAnswer(averageGrade);
 
 // 10.
 printAnswer(averageGrade > 80);
+
+// ARRAY MORE!!!!!!!!!
+
+// 0. Create an array
+const friends = [
+  "Elmo",
+  "Spongebob",
+  "CatDog",
+  "Danny Phantom",
+  "Timmy Turner",
+];
+
+function printFriends() {
+  document.querySelector("#extraAnswers").innerHTML += `<br /> ${friends}`;
+}
+
+// 1. Add items to an array using the push method
+friends.push("Jimmy Neutron");
+printFriends();
+
+// 2. Remove the last item to an array using the pop method
+friends.pop();
+printFriends();
+
+// 3. Remove the first item to an array using the slice method
+friends.splice(0, 1);
+printFriends();
+
+// 4. Add item to front of array using the
+friends.splice(0, 0, "Invader Zim");
+printFriends();
+
+// 5. Use splice again
+friends.splice(2, 1);
+printFriends();
+
+// 6. Splice
+friends.splice(2, 0, "Gir");
+printFriends();
+
+// 7. Splice
+friends.splice(3, 1, ["Patrick", "Squidward"]);
+printFriends();
+
+// splice method takes three parameters
+// 1. What index to start at
+// 2. How many elements starting from that index should be deleted
+// 3. What to add to the array
+//      This could be just a value or an array of values to add multiple
+
+// Objects
+const person = {
+  firstName: "Ty",
+  lastName: "the Pumpkin Guy",
+  age: 57,
+  hometown: "Naptown",
+  getFullName() {
+    return `${this.firstName} ${this.lastName}`;
+  },
+};
+
+// 1. Show full name
+document.getElementById("fullName").innerHTML += person.getFullName();
+
+// 2. Change value
+person.age += 1;
+document.getElementById("newFProp").innerHTML += person.age;
+
+// 3. Printing object variable using the JSON.stringify method
+document.getElementById("newFProp").innerHTML += `<br /> ${JSON.stringify(
+  person
+)}`;
+
+// 4. Remove property from person using delete keyword
+delete person.hometown;
+document.getElementById("newFProp").innerHTML += `<br /> ${JSON.stringify(
+  person
+)}`;
+
+// 5. Print a sentence using our object 
+console.log(`${person.getFullName()} is ${person.age} years old. `);
+
+// 6. Add a method to an existing object
+person.isASenior = function () {
+  return this.age >= 65; 
+};
+console.log(person.isASenior());
+
+person.age += 10;
+console.log(person.isASenior());
+
+let keyProp = "age";
+console.log(`person[keyProp] = ${person[keyProp]}`);
+console.log(`person.keyProp = ${personkeyProp}`);
+
+person["Middle Initial"] = "L";
+console.log(person["Middle Initial"]);
+
+person.hair_color = "black";
