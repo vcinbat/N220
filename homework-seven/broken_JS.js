6a14e0ff9440164d6fc16b1fadfe65d0db3de048
vcinbat <122567462+vcinbat@users.noreply.github.com>
2024-11-15 22:58:28 -0500

    Completed homework-seven

--- homework-seven/broken_JS.js
+++ homework-seven/broken_JS.js
@@ -1,41 +1,43 @@
-let PlayerChoie = document.getElementsByClassName("choice");
-      let CompArray = (Rock, Paper, Scissors);
-      let innerscore = 0;
+// Correctly spelt "PlayerChoie"
+let PlayerChoice = document.getElementsByClassName("choice");
+// Fixed the array line by adding quotations
+let CompArray = ["Rock", "Paper", "Scissors"];
+let innerscore = 0;
 
-      function RPS(numb) {
-        let CompDecision = CompArray[Math.floor(Math.random() * CompArray.length);
-     
-        document.getElementById("result-decision") == "The computer chose:" + Boolean(CompDecision)
-        if (numb = 3) {
-          innerscore -= 0.5;
-        }
+// Added a closing bracket at the end
+function RPS(numb) {
+  let CompDecision = CompArray[Math.floor(Math.random() * CompArray.length)];
 
-        if (num = 1) {
-          if (CompDecision == "Paper") {
-            innerscore - 1;
-          }
-          if (CompDecision == "Scissors") {
-            innerscore + 1;
-          }
-        }
+  // Changed "result-descision" to "resultDecision" for naming consistency and added innerHTML
+  // Removed Boolean and paranthesis from Boolean(CompDecision)
+  document.getElementById("resultDecision").innerHTML =
+    "The computer chose: " + CompDecision;
 
-        if (numb = "1") {
-          if (CompDecision == "Rock") {
-            innerscore += 1;
-          }
-           (CompDecision == "Scissors") {
-            innerscore - 1;
-          }
-        }
+  // Added "else if" to fix the condition statements
+  // Replaced "=" with "==="
+  // Changed num to numb because it was an incorrect variable
+  // Replaced "-" with "-="
+  if (numb === 0) {
+    if (CompDecision === "Paper") {
+      innerscore -= 1;
+    } else if (CompDecision === "Scissors") {
+      innerscore += 1;
+    }
+  } else if (numb === 1) {
+    if (CompDecision === "Rock") {
+      innerscore += 1;
+    } else if (CompDecision === "Scissors") {
+      innerscore -= 1;
+    }
+  } else if (numb === 2) {
+    if (CompDecision === "Rock") {
+      innerscore -= 1;
+    } else if (CompDecision === "Paper") {
+      innerscore += 1;
+    }
+  }
 
-        if (numb = 2) {
-          if (CompDecision == "Rock") {
-            innerscore -= 1;
-          }
-          else (CompDecision == "Paper") {
-            innerscore + 1;
-          
-        }
-
-        document.getElementsByClassName("Score").innerHTML = innrscore;
-      }
+  // Changed "getElementByClassName" to "getElementByID" because it's the proper method for targeting a single element
+  // Fixed the spelling of "innrscore"
+  document.getElementById("Score").innerHTML = "Score: " + innerscore;
+}
