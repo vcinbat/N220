e5077203993ba709798c40d64df09d19ea452d47
vcinbat <122567462+vcinbat@users.noreply.github.com>
2024-12-03 17:40:26 -0500

    Fixed the interest rate calculator

--- homework-three/app.js
+++ homework-three/app.js
@@ -1,16 +1,16 @@
 function calculateInterest() {
- 
+  // Retrieve user input values
   var principle = parseFloat(document.getElementById("principle").value);
-  var rate = parseFloat(document.getElementById("rate").value);
+  var rate = parseFloat(document.getElementById("rate").value) / 100; //Divide interest rate by 100
   var time = parseFloat(document.getElementById("time").value);
 
-  var totalInterest = principle * (1 + rate * time);
+  var totalInterest = principle * rate * time;
   var grandTotal = principle + totalInterest;
 
   var resultText = `
-With a beginning principal of $${principle.toFixed(
-    2
-  )} and with a growth rate of ${rate.toFixed(2)} for ${time} years, 
+With a beginning principal of $${principle.toFixed(2)} and a growth rate of ${(
+    rate * 100
+  ).toFixed(2)}% for ${time} years, 
 your total interest will be $${totalInterest.toFixed(
     2
   )} with a grand total of $${grandTotal.toFixed(2)}.
